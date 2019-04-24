d27374b855da86758211a117eade67e91cb24fee
agtechworld <sureshmunnangi@gmail.com>
2019-04-23 20:39:26 -0400

    commit

--- src/main/java/com/javasampleapproach/dynamodb/model/Product.java
+++ src/main/java/com/javasampleapproach/dynamodb/model/Product.java
@@ -1,4 +1,59 @@
 package com.javasampleapproach.dynamodb.model;
 
 public class Product {
+
+    private String productId;
+    private String productShortName;
+    private String productLongName;
+    private String originalPrice;
+    private String salesPrice;
+    private String productStatus;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductShortName() {
+        return productShortName;
+    }
+
+    public void setProductShortName(String productShortName) {
+        this.productShortName = productShortName;
+    }
+
+    public String getProductLongName() {
+        return productLongName;
+    }
+
+    public void setProductLongName(String productLongName) {
+        this.productLongName = productLongName;
+    }
+
+    public String getOriginalPrice() {
+        return originalPrice;
+    }
+
+    public void setOriginalPrice(String originalPrice) {
+        this.originalPrice = originalPrice;
+    }
+
+    public String getSalesPrice() {
+        return salesPrice;
+    }
+
+    public void setSalesPrice(String salesPrice) {
+        this.salesPrice = salesPrice;
+    }
+
+    public String getProductStatus() {
+        return productStatus;
+    }
+
+    public void setProductStatus(String productStatus) {
+        this.productStatus = productStatus;
+    }
 }
